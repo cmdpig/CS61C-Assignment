e0afe9d9e013809f21e8818a31f4de9f770e5137
cmdpig <114514.org>
2026-02-11 15:29:43 +0800

    121212

--- Lab/lab01/hello.c
+++ Lab/lab01/hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void f(){
+    int u=0;
+    int v=1;
+    u+v;
+
+}
+
+
 int main(int argc, char *argv[]) {
     int i;
     int count = 0;
@@ -8,7 +16,7 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < 10; i++) {
         (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
     }
-
+    f();
     printf("Thanks for waddling through this program. Have a nice day.");
     return 0;
 }
